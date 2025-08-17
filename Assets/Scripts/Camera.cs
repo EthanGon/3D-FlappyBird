29ae172b2e79c7fd01fbb4b4d80c391133c4cc05
Ethan Gonzalez <112298070+EthanGon@users.noreply.github.com>
2025-08-17 17:23:51 -0400

    adjusted camera code
    - changed up camera code to limit the x position to stop it from clipping outside the red zone

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -5,9 +5,21 @@ public class Camera : MonoBehaviour
     public Transform player;
     [SerializeField] private float camZOffset = 5.0f;
     [SerializeField] private float camYOffset = 5.0f;
+    [SerializeField] private float camXOffset = 5.0f;
+
+
+    [SerializeField] private float camXMin = 2.0f;
+    [SerializeField] private float camXMax = 2.0f;
+
 
     private void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + camYOffset, player.transform.position.z + camZOffset);
+        
+
+        if (player.position.x <= camXMax && player.position.x >= camXMin)
+        {
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y + camYOffset, player.transform.position.z + camZOffset);
+        }
+
     }
 }
